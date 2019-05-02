845f28dd8d9daecdf04dce86348ac87d69386d2f
Peter <yancong.li@alumni.ubc.ca>
2019-05-01 22:32:08 -0700

    revised based on code review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -40,7 +40,7 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          * 
@@ -55,9 +55,8 @@ $(function () {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function () {
-        /* TODO: Write a test that ensures the menu element is
+        /* A test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -65,27 +64,25 @@ $(function () {
         let bodyElement = document.querySelector('body');
 
         it('should be hidden by default', function () {
-            expect(bodyElement.getAttribute('class')).toContain('menu-hidden');
+            expect(bodyElement).toHaveClass('menu-hidden');
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /* A test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
         it('changes visibility when the menu icon is clicked', function () {
             $('.menu-icon-link').trigger("click");
-            expect(bodyElement.getAttribute('class')).toEqual('');
-            expect(bodyElement.getAttribute('class').length).toEqual(0);
+            expect(bodyElement).not.toHaveClass('menu-hidden');
 
             $('.menu-icon-link').trigger("click");
-            expect(bodyElement.getAttribute('class')).toContain('menu-hidden');
+            expect(bodyElement).toHaveClass('menu-hidden');
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function () {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -98,23 +95,13 @@ $(function () {
         });
 
         it('at least a single .entry element within the .feed container when the loadFeed() is called', function () {
-            const container = document.querySelector('.feed');
-            const childNodes = container.childNodes;
-            for (let aNode of childNodes) {
-                if (aNode.tagName === 'A') {
-                    for (let articleNode of aNode.childNodes) {
-                        if (articleNode.tagName === 'ARTICLE') {
-                            expect(articleNode.className).toEqual('entry');
-                        }
-                    }
-                }
-            }
+            const entryElements = document.querySelectorAll('.feed .entry');
+            expect(entryElements.length).toBeGreaterThan(0);
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function () {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
@@ -130,7 +117,6 @@ $(function () {
         it('is different', function (done) {
             loadFeed(1, function () {
                 domContent2 = document.querySelector('.feed');
-                expect(domContent1.childNodes.length).toEqual(domContent2.childNodes.length);
                 expect(domContent1).not.toEqual(domContent2);
                 expect(domContent1.childNodes[1]).not.toEqual(domContent2.childNodes[1]);
                 expect(domContent1.childNodes[1].getAttribute('href')).not.toEqual(domContent2.childNodes[1].getAttribute('href'));
